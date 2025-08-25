0b6367fe83c0e330943b9c7fb41977ca104eef83
Rojan Rajbhandari <rrojan20@gmail.com>
2025-08-25 15:13:35 +0545

    chore: enforce conventional commits

---
+++ commitlint.config.ts
@@ -0,0 +1,29 @@
+import type { UserConfig } from '@commitlint/types'
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  formatter: '@commitlint/format',
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // New feature
+        'fix', // Bug fix
+        'docs', // Documentation changes
+        'style', // Changes that do not affect the meaning of the code (white-space, formatting, etc.)
+        'refactor', // Code changes that neither fix a bug nor add a feature
+        'perf', // Performance improvement
+        'test', // Adding missing tests or correcting existing tests
+        'build', // Changes that affect the build system
+        'ci', // Changes to CI configuration files and scripts
+        'chore', // Other changes that don't modify src or test files
+        'revert', // Reverts a previous commit
+        'hotfix', // Hotfixes to main
+      ],
+    ],
+    'scope-enum': [2, 'always', ['OUT-*']], // Ticket number
+  },
+}
+
+export default Configuration
