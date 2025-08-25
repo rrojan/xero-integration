a73d6ad39ce04375938e101b4ea6b12fb6497062
Rojan Rajbhandari <rrojan20@gmail.com>
2025-08-25 23:09:34 +0545

    feat: setup client separation & app bridge

---
+++ src/client/hooks/app-bridge/useActionsMenu.tsx
@@ -0,0 +1,68 @@
+import { DASHBOARD_DOMAIN } from 'client/constants/domains'
+import type {
+  ActionsMenuPayload,
+  Clickable,
+  Configurable,
+} from 'client/hooks/app-bridge/types'
+import { ensureHttps } from 'client/utils/https'
+import { useEffect, useMemo } from 'react'
+
+const getActionMenuItemId = (idx: number) => `header.actionsMenu.${idx}`
+
+export function useActionsMenu(actions: Clickable[], config?: Configurable) {
+  const callbackRefs = useMemo(() => {
+    return actions.reduce<Record<string, () => void>>(
+      (acc, { onClick }, idx) => {
+        if (onClick) acc[getActionMenuItemId(idx)] = onClick
+        return acc
+      },
+      {},
+    )
+  }, [actions])
+
+  useEffect(() => {
+    const payload: ActionsMenuPayload = {
+      type: 'header.actionsMenu',
+      items: actions.map(({ label, onClick, icon, color }, idx) => ({
+        onClick: onClick ? getActionMenuItemId(idx) : '',
+        label,
+        icon,
+        color,
+      })),
+    }
+
+    window.parent.postMessage(payload, DASHBOARD_DOMAIN)
+    if (config?.portalUrl) {
+      window.parent.postMessage(payload, ensureHttps(config.portalUrl))
+    }
+
+    const handleMessage = (event: MessageEvent) => {
+      if (
+        event.data.type === 'header.actionsMenu.onClick' &&
+        typeof event.data.id === 'string' &&
+        callbackRefs[event.data.id]
+      ) {
+        callbackRefs[event.data.id]()
+      }
+    }
+
+    addEventListener('message', handleMessage)
+
+    return () => {
+      removeEventListener('message', handleMessage)
+    }
+  }, [actions, callbackRefs, config?.portalUrl])
+
+  useEffect(() => {
+    const handleUnload = () => {
+      window.parent.postMessage(
+        { type: 'header.actionsMenu', items: [] },
+        DASHBOARD_DOMAIN,
+      )
+    }
+    addEventListener('beforeunload', handleUnload)
+    return () => {
+      removeEventListener('beforeunload', handleUnload)
+    }
+  }, [])
+}
---
+++ src/client/hooks/app-bridge/usePrimaryCta.tsx
@@ -0,0 +1,62 @@
+import { DASHBOARD_DOMAIN } from 'client/constants/domains'
+import type {
+  Clickable,
+  Configurable,
+  PrimaryCtaPayload,
+} from 'client/hooks/app-bridge/types'
+import { ensureHttps } from 'client/utils/https'
+import { useEffect } from 'react'
+
+export const usePrimaryCta = (
+  primaryCta: Clickable | null,
+  config?: Configurable,
+) => {
+  useEffect(() => {
+    const payload: PrimaryCtaPayload | Pick<PrimaryCtaPayload, 'type'> =
+      !primaryCta
+        ? { type: 'header.primaryCta' }
+        : {
+            icon: primaryCta.icon,
+            label: primaryCta.label,
+            onClick: 'header.primaryCta.onClick',
+            type: 'header.primaryCta',
+          }
+
+    window.parent.postMessage(payload, DASHBOARD_DOMAIN)
+    if (config?.portalUrl) {
+      window.parent.postMessage(payload, ensureHttps(config.portalUrl))
+    }
+
+    const handleMessage = (event: MessageEvent) => {
+      if (
+        event.data.type === 'header.primaryCta.onClick' &&
+        typeof event.data.id === 'string' &&
+        primaryCta?.onClick
+      ) {
+        primaryCta.onClick()
+      }
+    }
+
+    addEventListener('message', handleMessage)
+
+    return () => {
+      removeEventListener('message', handleMessage)
+    }
+  }, [primaryCta, config?.portalUrl])
+
+  useEffect(() => {
+    const handleUnload = () => {
+      window.parent.postMessage({ type: 'header.primaryCta' }, DASHBOARD_DOMAIN)
+      if (config?.portalUrl) {
+        window.parent.postMessage(
+          { type: 'header.primaryCta' },
+          ensureHttps(config.portalUrl),
+        )
+      }
+    }
+    addEventListener('beforeunload', handleUnload)
+    return () => {
+      removeEventListener('beforeunload', handleUnload)
+    }
+  }, [config?.portalUrl])
+}
