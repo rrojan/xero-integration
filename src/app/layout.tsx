0f79d2519afaa4f01b71fb7ce791a79cacdbcc2f
Rojan Rajbhandari <rrojan20@gmail.com>
2025-08-28 19:06:54 +0545

    feat(OUT-2241): add initial auth screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import 'copilot-design-system/dist/styles/main.css'
 
 const inter = Inter({
   subsets: ['latin'],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Callout } from '@/components/Callout'
+
 export default function Home() {
   return (
-    <div className="text-center text-3xl py-8 text-text-secondary">
-      Xero Integration
-    </div>
+    <main className="pt-6 px-8 sm:px-[100px] lg:px-[220px] pb-[54px]">
+      <Callout
+        title={'Authorize your account'}
+        description={'Log into Xero with an admin account to get started.'}
+        variant={'info'}
+        actionProps={{
+          variant: 'primary',
+          label: 'Connect to Xero',
+          prefixIcon: 'Check',
+        }}
+      />
+    </main>
   )
 }
