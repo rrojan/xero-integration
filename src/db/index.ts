5e7dd2e8603eea3d42f725d11d626bd6bd4ef846
Rojan Rajbhandari <rrojan20@gmail.com>
2025-08-29 15:41:07 +0545

    chore(OUT-2241): implement singleton via global for db client

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,16 @@
+declare global {
+  var _drizzleDb: ReturnType<typeof drizzle> | undefined
+}
+
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import env from '@/config/server.env'
 
-const client = postgres(env.DATABASE_URL, { prepare: false })
-const db = drizzle({ client, casing: 'snake_case' })
+globalThis._drizzleDb ??= drizzle({
+  client: postgres(env.DATABASE_URL, { prepare: false }),
+  casing: 'snake_case',
+})
+
+const db = globalThis._drizzleDb
 
 export default db
