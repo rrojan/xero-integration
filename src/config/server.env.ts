db14c5270faf910d2f9419e60bf3e111e496b1b5
Rojan Rajbhandari <rrojan20@gmail.com>
2025-08-29 15:39:59 +0545

    feat(OUT-2241): add code to interop with Xero API via SDK

--- src/config/server.env.ts
+++ src/config/server.env.ts
@@ -1,10 +1,14 @@
+import 'server-only'
+
 import { z } from 'zod'
 
 const ServerEnvSchema = z.object({
-  COPILOT_API_KEY: z
-    .string({ error: 'Please provide a valid COPILOT_API_KEY' })
-    .min(1),
-  DATABASE_URL: z.url({ error: 'Please provide a valid DATABASE_URL' }),
+  COPILOT_API_KEY: z.string().min(1),
+  DATABASE_URL: z.url(),
+  XERO_CLIENT_ID: z.string().min(1),
+  XERO_CLIENT_SECRET: z.string().min(1),
+  XERO_CALLBACK_URL: z.url(),
+  XERO_SCOPES: z.string().min(1),
 })
 
 const env = ServerEnvSchema.parse(process.env)
