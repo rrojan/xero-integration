631b3e239e6842821331a91bf0341b3a77b008bc
Rojan Rajbhandari <rrojan20@gmail.com>
2025-08-30 10:45:06 +0545

    feat(OUT-2243): add build script

---
+++ scripts/build/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+echo "👷 Running build script for environment: ${VERCEL_ENV:-local}"
+
+# Validate that all required environment variables are set for environment
+pnpm tsx scripts/build/validateEnv.ts
+
+# Apply database migrations
+pnpm drizzle-kit migrate
+
+# Build the project with turbopack
+pnpm next build --turbopack
