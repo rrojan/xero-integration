fd412bc628bcbcfa0ca86e73ea25e0376ad18d9c
Rojan Rajbhandari <rrojan20@gmail.com>
2025-09-01 17:39:35 +0545

    feat(OUT-2243): add basic homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,32 @@
-import { Callout } from '@/components/Callout'
-import { SilentError } from '@/components/templates/SilentError'
-import type { PageProps } from '@/types/componentProps'
+import type { PageProps } from '@/app/types'
+import { AppStateContextProvider } from '@/context/AppContext'
+import { serializeClientUser } from '@/features/copilot-integration/models/ClientUser.model'
+import User from '@/features/copilot-integration/models/User.model'
+import { CalloutSection } from '@/features/xero-integration/auth/components/CalloutSection'
+import { RealtimeXeroConnections } from '@/features/xero-integration/auth/components/RealtimeXeroConnections'
+import { TempSyncStatus } from '@/features/xero-integration/auth/components/TempSyncStatus'
+import XeroConnectionsService from '@/features/xero-integration/auth/lib/XeroConnections.service'
 
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
 const Home = async ({ searchParams }: PageProps) => {
-  const { token } = await searchParams
+  const sp = await searchParams
+  const user = await User.authenticate(sp.token)
 
-  if (!token) {
-    return <SilentError message="No token available" />
-  }
+  const xeroConnectionsService = new XeroConnectionsService(user)
+  const xeroConection = await xeroConnectionsService.getConnectionForWorkspace()
+
+  const clientUser = serializeClientUser(user)
 
   return (
-    <main className="pt-6 px-8 sm:px-[100px] lg:px-[220px] pb-[54px]">
-      <Callout
-        title={'Authorize your account'}
-        description={'Log into Xero with an admin account to get started.'}
-        variant={'info'}
-        actionProps={{
-          variant: 'primary',
-          label: 'Connect to Xero',
-          prefixIcon: 'Check',
-        }}
-        hrefUrl={`/auth/initiate?token=${token}`}
-      />
-    </main>
+    <AppStateContextProvider user={clientUser} connectionStatus={!!xeroConection?.status}>
+      <main className="pt-6 px-8 sm:px-[100px] lg:px-[220px] pb-[54px]">
+        <RealtimeXeroConnections user={clientUser} />
+        <CalloutSection />
+        <TempSyncStatus />
+      </main>
+    </AppStateContextProvider>
   )
 }
 
