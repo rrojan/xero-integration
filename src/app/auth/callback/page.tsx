472ff40d5a808c46ee83251c9dceff945a656b14
Rojan Rajbhandari <rrojan20@gmail.com>
2025-09-01 17:37:59 +0545

    feat(OUT-2243): implement callback receiver page

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,11 +1,9 @@
 import Script from 'next/script'
-import { z } from 'zod'
-import { SilentError } from '@/components/templates/SilentError'
-import db from '@/db'
-import { xeroConnections } from '@/db/schema/xeroConnections.schema'
-import { CopilotAPI } from '@/lib/CopilotAPI'
-import xero from '@/lib/XeroAPI'
-import type { XeroTokenSet } from '@/types/xeroApi'
+import { SilentError } from '@/components/layouts/SilentError'
+import User from '@/features/copilot-integration/models/User.model'
+import XeroConnectionsService from '@/features/xero-integration/auth/lib/XeroConnections.service'
+import type { XeroTokenSet } from '@/features/xero-integration/lib/types'
+import XeroAPI from '@/features/xero-integration/lib/XeroAPI'
 
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
@@ -16,45 +14,20 @@ interface CallbackPageProps {
 
 const CallbackPage = async ({ searchParams }: CallbackPageProps) => {
   const sp = await searchParams
-  const token = z.string().parse(sp.state)
-  delete sp.state // xero-node sdk doesn't expect state in the url params
-
-  if (!token) {
-    return <SilentError message="No token available" />
-  }
-
-  const copilot = new CopilotAPI(token)
-  const tokenPayload = await copilot.getTokenPayload()
-
-  if (!tokenPayload || !tokenPayload.workspaceId || !tokenPayload.internalUserId) {
-    return <SilentError message="Invalid token payload" />
-  }
+  const user = await User.authenticate(sp.state)
 
   let tokenSet: XeroTokenSet
   try {
+    delete sp.state // xero-node sdk doesn't expect state in the url params
+    const xero = new XeroAPI()
     tokenSet = await xero.handleApiCallback(sp)
   } catch (error) {
     console.error('Error handling Xero callback:', error)
     return <SilentError message="Failed to handle Xero callback" />
   }
 
-  // Upsert xero connection record
-  await db
-    .insert(xeroConnections)
-    .values({
-      portalId: z.string().min(1).parse(tokenPayload.workspaceId),
-      tokenSet: tokenSet as XeroTokenSet,
-      status: true,
-      initiatedBy: tokenPayload.internalUserId,
-    })
-    .onConflictDoUpdate({
-      target: xeroConnections.portalId,
-      set: {
-        tokenSet: tokenSet as XeroTokenSet,
-        status: true,
-        initiatedBy: tokenPayload.internalUserId,
-      },
-    })
+  const xeroConnectionsService = new XeroConnectionsService(user)
+  await xeroConnectionsService.upsertConnection(tokenSet)
 
   return (
     <div className="py-4 px-2">
