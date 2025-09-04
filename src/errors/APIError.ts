e466b5b1fcd69ef00809e929b78139c67d9b645a
Rojan Rajbhandari <rrojan20@gmail.com>
2025-09-04 12:47:43 +0545

    chore(OUT-2295): add APIError

---
+++ src/errors/APIError.ts
@@ -0,0 +1,14 @@
+import { BaseServerError } from '@/errors/BaseServerError'
+
+class APIError extends BaseServerError {
+  constructor(
+    message: string,
+    public readonly status: number = 500,
+    public readonly error?: unknown[],
+  ) {
+    super(message, status)
+    this.name = 'APIError'
+  }
+}
+
+export default APIError
