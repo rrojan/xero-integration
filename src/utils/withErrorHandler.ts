d85101a72263933095bc8c50f0fb80de61bbe9f0
Rojan Rajbhandari <rrojan20@gmail.com>
2025-09-04 12:46:00 +0545

    chore(OUT-2295): add withErrorHandler HOC

---
+++ src/utils/withErrorHandler.ts
@@ -0,0 +1,59 @@
+import httpStatus from 'http-status'
+import { type NextRequest, NextResponse } from 'next/server'
+import z, { ZodError } from 'zod'
+import APIError from '@/errors/APIError'
+import type { StatusableError } from '@/errors/BaseServerError'
+
+type RequestHandler = (req: NextRequest, params: unknown) => Promise<NextResponse>
+
+/**
+ * Reusable utility that wraps a given request handler with a global error handler to standardize response structure
+ * in case of failures. Catches exceptions thrown from the handler, and returns a formatted error response.
+ *
+ * @param {RequestHandler} handler - The request handler to wrap.
+ * @returns {RequestHandler} The new handler that includes error handling logic.
+ * @example
+ * const safeHandler = withErrorHandler(async (req: NextRequest) => {
+ *   // your request handling logic
+ *   if (errorCondition) {
+ *     throw new Error("Oh no!")}
+ *   return NextResponse.next();
+ * });
+ *
+ * @throws {ZodError} Captures and handles validation errors and responds with status 400 and the issue detail.
+ * @throws {APIError} Captures and handles APIError
+ */
+export const withErrorHandler = (handler: RequestHandler): RequestHandler => {
+  return async (req: NextRequest, params: unknown) => {
+    // Execute the handler wrapped in a try... catch block
+    try {
+      return await handler(req, params)
+    } catch (error: unknown) {
+      // Log error in a readable way
+      if (error instanceof ZodError) {
+        const formattedError = z.treeifyError(error)
+        console.error('ZodError: ', z.prettifyError(error), '\n', formattedError)
+      } else {
+        console.error(error)
+      }
+
+      // Build error API response
+      let status: number = (error as StatusableError).status || httpStatus.INTERNAL_SERVER_ERROR
+      let message = 'Something went wrong'
+
+      // Build a proper response based on the type of Error encountered
+      if (error instanceof ZodError) {
+        status = httpStatus.UNPROCESSABLE_ENTITY
+        message = z.prettifyError(error)
+      } else if (error instanceof APIError) {
+        status = error.status
+        message = error.message || message
+        console.error(error.error)
+      } else if (error instanceof Error && error.message) {
+        message = error.message
+      }
+
+      return NextResponse.json({ error: message }, { status })
+    }
+  }
+}
