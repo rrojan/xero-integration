9a2139d2275cb3c4b97c948792442ae5a4d8ce30
Rojan Rajbhandari <rrojan20@gmail.com>
2025-09-08 19:41:26 +0545

    feat(OUT-2297): implement contact saving & mapping

--- src/features/invoice-sync/lib/XeroInvoiceSync.service.ts
+++ src/features/invoice-sync/lib/XeroInvoiceSync.service.ts
@@ -3,43 +3,32 @@ import 'server-only'
 import type { InvoiceCreatedEvent } from '@invoice-sync/types'
 import { Invoice, LineAmountTypes } from 'xero-node'
 import AuthenticatedXeroService from '@/lib/xero/AuthenticatedXero.service'
-import { type CreateInvoicePayload, CreateInvoicePayloadSchema } from '@/lib/xero/types'
+import { CreateInvoicePayloadSchema } from '@/lib/xero/types'
 import { datetimeToDate } from '@/utils/date'
 import { serializeLineItems } from './serializer'
+import XeroContactService from './XeroContact.service'
 
 class XeroInvoiceSyncService extends AuthenticatedXeroService {
   async syncInvoiceToXero(data: InvoiceCreatedEvent) {
+    // Prepare invoid payload fields
     const lineItems = serializeLineItems(data.lineItems)
+    const xeroContactService = new XeroContactService(this.user, this.connection)
+    const { contactID } = await xeroContactService.getSyncedXeroContact(data.clientId)
+
     const invoice = CreateInvoicePayloadSchema.parse({
-      invoiceNumber: data.number,
       type: Invoice.TypeEnum.ACCREC,
-      // TODO: Client ID is static for now, will be handled in upcoming ticket
-      contact: { contactID: 'e74c821d-422a-42eb-926f-dd5db199bb81' },
+      invoiceNumber: data.number,
+      contact: { contactID },
       dueDate: datetimeToDate(data.dueDate),
       lineAmountTypes: LineAmountTypes.Exclusive,
       lineItems,
       status: Invoice.StatusEnum.SUBMITTED,
       date: datetimeToDate(data.sentDate),
-    } satisfies CreateInvoicePayload)
+    } satisfies Invoice)
 
-    const syncedInvoice = await this.xero.createInvoice(this.connection.tenantId, [invoice])
+    const syncedInvoice = await this.xero.createInvoices(this.connection.tenantId, [invoice])
     console.info(syncedInvoice)
   }
-
-  // invoices: [
-  //   {
-  //     type: Invoice.TypeEnum.ACCREC,
-  //     contact: {
-  //       contactID: 'e74c821d-422a-42eb-926f-dd5db199bb81',
-  //     },
-  //     dueDate: '2026-06-06',
-  //     lineItems: [
-  //       {
-  //       },
-  //     ],
-  //     status: Invoice.StatusEnum.AUTHORISED,
-  //   },
-  // ],
 }
 
 export default XeroInvoiceSyncService
