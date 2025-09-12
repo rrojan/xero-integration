cba00f8f29cc86edf510c2f57c8b01c7ac424efb
Rojan Rajbhandari <rrojan20@gmail.com>
2025-09-12 15:23:16 +0545

    chore: add synced_invoices table

---
+++ src/db/migrations/20250912081800_add_synced_invoices_table.sql
@@ -0,0 +1,13 @@
+CREATE TYPE "public"."status" AS ENUM('pending', 'failed', 'success');
+CREATE TABLE "synced_invoices" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"portal_id" varchar(16) NOT NULL,
+	"copilot_invoice_id" varchar(64) NOT NULL,
+	"xero_invoice_id" uuid,
+	"status" "status" DEFAULT 'pending' NOT NULL,
+	"tenant_id" uuid NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+
+CREATE UNIQUE INDEX "uq_synced_invoices_portal_id_copilot_invoice_id" ON "synced_invoices" USING btree ("portal_id","copilot_invoice_id");
