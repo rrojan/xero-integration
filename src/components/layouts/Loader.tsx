3128c8dc8df11d4a1a53e4c4e02f4cbeeba7aed5
Rojan Rajbhandari <rrojan20@gmail.com>
2025-09-12 12:43:16 +0545

    fix(OUT-2336): add accessibility for spinner

--- src/components/layouts/Loader.tsx
+++ src/components/layouts/Loader.tsx
@@ -4,7 +4,11 @@ import { Spinner } from 'copilot-design-system'
 
 export const Loader = () => {
   return (
-    <div className="flex flex-col items-center justify-center pb-[138px] min-h-[100vh]">
+    // biome-ignore lint/a11y/useSemanticElements: output tag is not semantic here
+    <div
+      role="status"
+      className="flex flex-col items-center justify-center pb-[138px] min-h-[100vh]"
+    >
       <Spinner size={10} />
     </div>
   )
