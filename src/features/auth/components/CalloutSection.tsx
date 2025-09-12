ec2e0c5eb4c344b21a93ecf283e862b69a46559d
Rojan Rajbhandari <rrojan20@gmail.com>
2025-09-12 12:39:07 +0545

    feat(OUT-2336): better auth realtime handling

--- src/features/auth/components/CalloutSection.tsx
+++ src/features/auth/components/CalloutSection.tsx
@@ -4,20 +4,22 @@ import { Callout } from '@/components/ui/Callout'
 import { useAppState } from '@/context/AppContext'
 
 export const CalloutSection = () => {
-  const { user } = useAppState()
+  const { user, connectionStatus } = useAppState()
 
-  // TODO: Static for now...
-  return (
-    <Callout
-      title={'Authorize your account'}
-      description={'Log into Xero with an admin account to get started.'}
-      variant={'info'}
-      actionProps={{
-        variant: 'primary',
-        label: 'Connect to Xero',
-        prefixIcon: 'Check',
-      }}
-      hrefUrl={`/auth/initiate?token=${user.token}`}
-    />
-  )
+  if (!connectionStatus)
+    return (
+      <Callout
+        title={'Authorize your account'}
+        description={'Log into Xero with an admin account to get started.'}
+        variant={'info'}
+        actionProps={{
+          variant: 'primary',
+          label: 'Connect to Xero',
+          prefixIcon: 'Check',
+        }}
+        hrefUrl={`/auth/initiate?token=${user.token}`}
+      />
+    )
+
+  return <div className="text-2xl">Connected!</div>
 }
